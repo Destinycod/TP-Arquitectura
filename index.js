c4c6dee0c41a21bee91eb5bb124848f4029243ed
Clara Rodriguez <clara.rodriguez.a@hotmail.com>
2024-06-05 20:15:01 -0300

    ultimos arreglos

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require("./instrument");
+
+const Sentry = require("@sentry/node");
+//const Tracing = require("@sentry/tracing");
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
@@ -8,11 +12,29 @@ const userRoute = require("./routes/user");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+/*
+// Initialize Sentry
+Sentry.init({
+    dsn: process.env.SENTRY_DSN, // Coloca tu DSN aquí
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });*/
 
+// Usar Sentry para capturar todas las excepciones
+//app.use(Sentry.Handlers.requestHandler());
+//app.use(Sentry.Handlers.tracingHandler());
+
+// Conexión a la base de datos
 mongoose.connect(process.env.MONGO_URL)
-.then(()=> console.log("DB connection successfull"))
-.catch((error)=>{
+  .then(() => console.log("DB connection successful"))
+  .catch((error) => {
     console.log(error);
+    Sentry.captureException(error); // Capture MongoDB connection errors
 });
 
 app.use(express.json());
@@ -21,10 +43,23 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+  
+// Agrega el manejador de errores de Sentry
+//app.use(Sentry.Handlers.errorHandler());
+
+// Ruta de ejemplo para verificar que Sentry está funcionando
+app.get('/debug-sentry', function mainHandler(req, res) {
+    throw new Error('My first Sentry error!');
+});
+
+//console.log(Sentry);
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`);
     V1SwaggerDocs(app, process.env.PORT);
 });
 
-module.exports = app;
+//module.exports = app;
