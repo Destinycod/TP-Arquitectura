85ea8dfee8aeb8399040624d3748f794c7980395
Clara Rodriguez <clara.rodriguez.a@hotmail.com>
2024-06-12 20:43:10 -0300

    se agrega el archivo de configuración de Jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+    modulePathIgnorePatterns: [
+      "<rootDir>/node_modules/@sentry/profiling-node/lib/sentry_cpu_profiler-win32-x64-83.node"
+    ],
+    transformIgnorePatterns: [
+      "node_modules/@sentry/profiling-node"
+    ],
+  };
